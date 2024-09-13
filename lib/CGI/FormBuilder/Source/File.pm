9b12fa5122a803697fef631e8993fc95d71e0dc4
Mark Hedges <11917630+hedges333@users.noreply.github.com>
2024-09-12 18:20:11 -0700

    Bug# 114018 utf8::decode() instead of Encode.pm

--- lib/CGI/FormBuilder/Source/File.pm
+++ lib/CGI/FormBuilder/Source/File.pm
@@ -28,6 +28,7 @@ no  warnings 'uninitialized';
 
 use 5.006; # or later
 use CGI::FormBuilder::Util;
+use utf8;
 
 
 our $VERSION = '3.10';
@@ -78,7 +79,7 @@ sub parse {
         chomp;
         my($term, $line) = split /\s*:\s*/, $_, 2;
         $utf8 = 1 if $term eq 'charset' && $line =~ /^utf/;  # key off charset to decode value
-        $line = Encode::decode('utf-8', $line) if $utf8;
+        $line = utf8::decode($line) if $utf8;
 
         # here string term-inator (har)
         if ($here) {
